7f655fc7e682480c14dcc768e3e3450bc9fc2be9
IN2DM3B_10 <IN2DM3B_10@H015112.NET>
2019-11-05 08:58:18 +0100

    Modificación búsqueda empleados por nombre para varios resultados

--- src/main/java/controlador/ControladorVerEmpleados.java
+++ src/main/java/controlador/ControladorVerEmpleados.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
@@ -27,6 +28,7 @@ public class ControladorVerEmpleados {
 	public void initListeners() {
 		vista.pCentral.pVerEmple.btnBuscarDNI.addActionListener(new listenerBotones());
 		vista.pCentral.pVerEmple.btnBuscarNombre.addActionListener(new listenerBotones());
+		vista.pCentral.pVerEmple.btnVerEmple.addActionListener(new listenerBotones());
 	}
 	
 	private class listenerBotones implements ActionListener {
@@ -34,10 +36,21 @@ public class ControladorVerEmpleados {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			switch (e.getActionCommand()) {
-				case "buscarDNI": buscarEmpleado("DNI", vista.pCentral.pVerEmple.txtBusqDNI.getText());
+				case "buscarDNI": vista.pCentral.pVerEmple.txtBusqNombre.setText(""); 
+					buscarEmpleado("DNI", vista.pCentral.pVerEmple.txtBusqDNI.getText());
 					break;
-				case "buscarNombre": buscarEmpleado("Nombre", vista.pCentral.pVerEmple.txtBusqNombre.getText());
+				case "buscarNombre": vista.pCentral.pVerEmple.txtBusqDNI.setText(""); 
+					buscarEmpleado("Nombre", vista.pCentral.pVerEmple.txtBusqNombre.getText());
 					break;
+				case "ver": int index = vista.pCentral.pVerEmple.listEmpleados.getSelectedIndex();
+					if (index != -1) {
+					for (Empleado emple: modelo.empleados) {
+						if(emple.getDni().equals(vista.pCentral.pVerEmple.listModel.getElementAt(index).substring(0,9))) {
+							verDatosEmple(emple);
+							break;
+						}
+					}
+				}
 			}			
 		}
 		
@@ -58,41 +71,53 @@ public class ControladorVerEmpleados {
 	}
 	
 	private void buscarEmpleado(String campo, String valor) {
-		Empleado emple = null;
+		ArrayList<Empleado> emples = new ArrayList<Empleado>();
+		
+		vista.pCentral.pVerEmple.listModel.removeAllElements();
+		vista.pCentral.pVerEmple.listEmpleados.setModel(vista.pCentral.pVerEmple.listModel);
 		
 		if (campo.equals("DNI")) {
 			for (Empleado e: modelo.empleados) {
 				if (e.getDni().contentEquals(valor))
-					emple = e;
+					emples.add(e);
+			}
+			
+			if (emples.size() > 0) {
+				verDatosEmple(emples.get(0));
+			} else {
+				JOptionPane.showMessageDialog(vista, "No existe ningún empleado con ese DNI", "Información", JOptionPane.INFORMATION_MESSAGE);
 			}
 		} else if (campo.equals("Nombre")) {
 			for (Empleado e: modelo.empleados) {
-				if (e.getNombre().equals(valor))
-					emple = e;
+				if (e.getNombre().equals(valor)) {
+					emples.add(e);
+					vista.pCentral.pVerEmple.listModel.addElement(e.getDni() + " " + e.getNombre());
+				}
 			}
+			vista.pCentral.pVerEmple.listEmpleados.setModel(vista.pCentral.pVerEmple.listModel);
 		}
 
 		
+		
+	}
+	
+	private void verDatosEmple(Empleado emple) {
 		String esJefe;
-		if (emple != null) {	
-			if (emple.isJefe())
-				esJefe = "SI";
-			else
-				esJefe = "NO";
-			
-			vista.pCentral.pVerEmple.lblDNI.setText(emple.getDni());
-			vista.pCentral.pVerEmple.lblNombre.setText(emple.getNombre());
-			vista.pCentral.pVerEmple.lblSueldo.setText(formatoMoneda.format(emple.getSueldo()));
-			vista.pCentral.pVerEmple.lblCargo.setText(emple.getCargo().getNombre());
-			vista.pCentral.pVerEmple.lblDpto.setText(emple.getDepartamento().getNombre());
-			if (emple.getResponsable() != null)
-				vista.pCentral.pVerEmple.lblResponsable.setText(emple.getResponsable().getNombre());
-			vista.pCentral.pVerEmple.lblFecha.setText(formatoFecha.format(emple.getFecha()));
-			vista.pCentral.pVerEmple.lblJefe.setText(esJefe);
-		} else {
-			JOptionPane.showMessageDialog(this.vista, "No existen coinidencias en la base de datos", "Información", JOptionPane.INFORMATION_MESSAGE);
-		}
-
+		
+		if (emple.isJefe())
+			esJefe = "SI";
+		else
+			esJefe = "NO";
+		
+		vista.pCentral.pVerEmple.lblDNI.setText(emple.getDni());
+		vista.pCentral.pVerEmple.lblNombre.setText(emple.getNombre());
+		vista.pCentral.pVerEmple.lblSueldo.setText(formatoMoneda.format(emple.getSueldo()));
+		vista.pCentral.pVerEmple.lblCargo.setText(emple.getCargo().getNombre());
+		vista.pCentral.pVerEmple.lblDpto.setText(emple.getDepartamento().getNombre());
+		if (emple.getResponsable() != null)
+			vista.pCentral.pVerEmple.lblResponsable.setText(emple.getResponsable().getNombre());
+		vista.pCentral.pVerEmple.lblFecha.setText(formatoFecha.format(emple.getFecha()));
+		vista.pCentral.pVerEmple.lblJefe.setText(esJefe);
 	}
 
 }
--- src/main/java/vista/PanelVerEmpleados.java
+++ src/main/java/vista/PanelVerEmpleados.java
@@ -3,12 +3,15 @@ package vista;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+
+import modelo.Empleado;
 
 public class PanelVerEmpleados extends JPanel {
 
@@ -18,10 +21,12 @@ public class PanelVerEmpleados extends JPanel {
 	private static final long serialVersionUID = 9L;
 	
 	private  JLabel lblTituloEmple, lblBsquedaDNI, lblBsquedaNombre;
-	public JButton btnBuscarDNI, btnBuscarNombre;
+	public JButton btnBuscarDNI, btnBuscarNombre, btnVerEmple;
 	public JTextField txtBusqDNI, txtBusqNombre;
 	private JLabel lblTituloDNI, lblTituloNombre, lblTituloSueldo, lblTituloCargo, lblTituloDepartamento, lblTituloJefe, lblTituloResponsable, lblTituloFechaDeAlta;
 	public JLabel lblDNI, lblNombre, lblSueldo, lblCargo, lblJefe, lblResponsable, lblDpto, lblFecha;
+	public DefaultListModel<String> listModel = new DefaultListModel<String>();
+	public JList<String> listEmpleados;
 
 	/**
 	 * Create the panel.
@@ -43,116 +48,127 @@ public class PanelVerEmpleados extends JPanel {
 		
 		btnBuscarDNI = new JButton("Buscar por DNI");
 		btnBuscarDNI.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnBuscarDNI.setBounds(65, 150, 200, 30);
+		btnBuscarDNI.setBounds(66, 136, 200, 30);
 		btnBuscarDNI.setActionCommand("buscarDNI");
 		add(btnBuscarDNI);
 		
 		btnBuscarNombre = new JButton("Buscar por Nombre");
 		btnBuscarNombre.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnBuscarNombre.setBounds(65, 300, 200, 30);
+		btnBuscarNombre.setBounds(66, 256, 200, 30);
 		btnBuscarNombre.setActionCommand("buscarNombre");
 		add(btnBuscarNombre);
 		
 		lblBsquedaDNI = new JLabel("Búsqueda por DNI:");
 		lblBsquedaDNI.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblBsquedaDNI.setBounds(65, 80, 150, 30);
+		lblBsquedaDNI.setBounds(66, 66, 150, 30);
 		add(lblBsquedaDNI);
 		
 		lblBsquedaNombre = new JLabel("Búsqueda por nombre:");
 		lblBsquedaNombre.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblBsquedaNombre.setBounds(65, 230, 150, 30);
+		lblBsquedaNombre.setBounds(66, 186, 150, 30);
 		add(lblBsquedaNombre);
 		
 		txtBusqDNI = new JTextField();
 		txtBusqDNI.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		txtBusqDNI.setBounds(65, 110, 200, 30);
+		txtBusqDNI.setBounds(66, 96, 200, 30);
 		add(txtBusqDNI);
 		txtBusqDNI.setColumns(10);
 		
 		txtBusqNombre = new JTextField();
 		txtBusqNombre.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtBusqNombre.setColumns(10);
-		txtBusqNombre.setBounds(65, 260, 200, 30);
+		txtBusqNombre.setBounds(66, 216, 200, 30);
 		add(txtBusqNombre);
 		
 		lblTituloDNI = new JLabel("DNI");
 		lblTituloDNI.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblTituloDNI.setBounds(350, 80, 94, 30);
+		lblTituloDNI.setBounds(378, 79, 94, 30);
 		add(lblTituloDNI);
 		
 		lblTituloNombre = new JLabel("Nombre");
 		lblTituloNombre.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblTituloNombre.setBounds(350, 120, 94, 30);
+		lblTituloNombre.setBounds(378, 119, 94, 30);
 		add(lblTituloNombre);
 		
 		lblTituloSueldo = new JLabel("Sueldo");
 		lblTituloSueldo.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblTituloSueldo.setBounds(350, 160, 94, 30);
+		lblTituloSueldo.setBounds(378, 159, 94, 30);
 		add(lblTituloSueldo);
 		
 		lblTituloCargo = new JLabel("Cargo");
 		lblTituloCargo.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblTituloCargo.setBounds(350, 240, 94, 30);
+		lblTituloCargo.setBounds(378, 239, 94, 30);
 		add(lblTituloCargo);
 		
 		lblTituloDepartamento = new JLabel("Departamento");
 		lblTituloDepartamento.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblTituloDepartamento.setBounds(350, 200, 94, 30);
+		lblTituloDepartamento.setBounds(378, 199, 94, 30);
 		add(lblTituloDepartamento);
 		
 		lblTituloJefe = new JLabel("Jefe");
 		lblTituloJefe.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblTituloJefe.setBounds(350, 320, 94,30);
+		lblTituloJefe.setBounds(378, 319, 94,30);
 		add(lblTituloJefe);
 		
 		lblTituloResponsable = new JLabel("Responsable");
 		lblTituloResponsable.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblTituloResponsable.setBounds(350, 280, 94, 30);
+		lblTituloResponsable.setBounds(378, 279, 94, 30);
 		add(lblTituloResponsable);
 		
 		lblJefe = new JLabel("SI");
 		lblJefe.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblJefe.setBounds(488, 320, 250, 30);
+		lblJefe.setBounds(516, 319, 38, 30);
 		add(lblJefe);
 		
 		lblTituloFechaDeAlta = new JLabel("Fecha de alta");
 		lblTituloFechaDeAlta.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblTituloFechaDeAlta.setBounds(350, 360, 94, 30);
+		lblTituloFechaDeAlta.setBounds(378, 359, 94, 30);
 		add(lblTituloFechaDeAlta);
 		
 		lblDNI = new JLabel("12345678X");
 		lblDNI.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblDNI.setBounds(488, 80, 200, 30);
+		lblDNI.setBounds(516, 79, 200, 30);
 		add(lblDNI);
 		
 		lblNombre = new JLabel("Nombre");
 		lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNombre.setBounds(488, 120, 200, 30);
+		lblNombre.setBounds(516, 119, 200, 30);
 		add(lblNombre);
 		
 		lblSueldo = new JLabel("50000");
 		lblSueldo.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblSueldo.setBounds(488, 160, 200, 30);
+		lblSueldo.setBounds(516, 159, 200, 30);
 		add(lblSueldo);
 		
 		lblDpto = new JLabel("Departamento");
 		lblDpto.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblDpto.setBounds(488, 200, 200, 30);
+		lblDpto.setBounds(516, 199, 200, 30);
 		add(lblDpto);
 		
 		lblCargo = new JLabel("Cargo");
 		lblCargo.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblCargo.setBounds(488, 240, 200, 30);
+		lblCargo.setBounds(516, 239, 200, 30);
 		add(lblCargo);
 		
 		lblResponsable = new JLabel("Responsable");
 		lblResponsable.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblResponsable.setBounds(488, 280, 200, 30);
+		lblResponsable.setBounds(516, 279, 200, 30);
 		add(lblResponsable);
 		
 		lblFecha = new JLabel("Fecha");
 		lblFecha.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblFecha.setBounds(488, 360, 200, 30);
+		lblFecha.setBounds(516, 359, 200, 30);
 		add(lblFecha);
+		
+		listEmpleados = new JList();
+		listEmpleados.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		listEmpleados.setBounds(66, 297, 270, 74);
+		add(listEmpleados);
+		
+		btnVerEmple = new JButton("Ver");
+		btnVerEmple.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnVerEmple.setBounds(141, 371, 120, 30);
+		btnVerEmple.setActionCommand("ver");
+		add(btnVerEmple);
 	}
 }
